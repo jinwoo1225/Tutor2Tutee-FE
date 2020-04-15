bda8a14c7a995e58a04b0b16d250c88e3740bd71
Jinwoo <jinwoo@Jinwoos-MacBookPro.local>
2020-04-15 16:43:40 +0900

    fun with about.js

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function About(){
+    const [text, setText] = useState("")
+
+    setInterval(() => {
+        setText(Date.now())
+    }, 100000);
+
     return(
-        <h2>This is about</h2>
+        <h2>This is about: {text}</h2>
     )
 }
 
