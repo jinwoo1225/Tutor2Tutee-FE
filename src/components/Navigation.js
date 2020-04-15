0e2a03e2dc3a820fa37ca39354e09363a8198221
Jinwoo <jinwoo@Jinwoos-MacBookPro.local>
2020-04-15 15:45:43 +0900

    added login Navigationjs

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,33 +4,35 @@ import {login, logout} from '../store'
 import { connect } from 'react-redux';
 
 function Navigation({loginState, props, dlogin, dlogout}){
-    const [classID, setClassId] = useState("");
+    const [classID, setClassID] = useState("");
+    const [userID, setUserID] = useState("");
 
-    const onChangeClassID = e => {
-        setClassId(e.target.value);
-    }
+    const onChangeClassID = e => {setClassID(e.target.value);}
+    const onChangeUserID = e => {setUserID(e.target.value)};
 
-    const onSubmitClassID = e =>{
-        e.preventDefault();
-        
-        setClassId("");
-    }
-    const onClickLogin = () => {dlogin("Hello")}
-    const onClickLogout = () => {dlogout()}
+    const onClickLogin = () => {dlogin(userID)};
+    const onClickLogout = () => {dlogout()};
     
 
     return(<>
-            <h1>This is Navigation Bar</h1>
-            <h2> user.id : {loginState.id}</h2>
-            <form onSubmit={onSubmitClassID}>
+            <h3>This is Navigation Bar </h3>
+            <p>Hello {loginState.id}</p>
+            <form>
                 {/* 클래스로 이동 */}
                 <input type='text' value={classID} placeholder="Class ID" onChange={onChangeClassID}/>
                 <Link to={`/class/${classID}`}><button>Class</button></Link>
             </form>
+            <form>
+                {/* 로그인 */}
+                <input type='text' value={userID} placeholder="User ID" onChange={onChangeUserID}/>
+                <button onClick={onClickLogin}>Login</button>
+            </form>
+
+            <Link to={'/'}><button>Home</button></Link>
             <Link to={'/about'}><button>About</button></Link>
 
-            {/* 로그인 */}
-            <button onClick={onClickLogin}>Login</button>
+            
+            
             <button onClick={onClickLogout}>Logout</button>
         </>
     )
