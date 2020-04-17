36ae4c00266db4fdfb288507a96a62fb343edcba
jinwoo1225 <jinwoo17962@gmail.com>
2020-04-17 13:34:44 +0900

    fixed login.js without post function

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -4,15 +4,25 @@ import { connect } from 'react-redux';
 
 function Login({dlogin, dlogout}) {
     const [userID, setUserID] = useState("");
+    const [userPW, setPassword] = useState("");
     const onChangeUserID = e => {setUserID(e.target.value)};
+    const onChangePasswd = e => {setPassword(e.target.value)};
 
     const onClickLogin = () => {dlogin(userID)};
+
+    const onSubmit = e => {
+        console.log({
+            userID,
+            userPW
+        })
+    }
     return(
     <>
         <h1>This is Login page</h1>
-        <form>
+        <form onSubmit={onSubmit}>
             {/* 로그인 */}
             <input type='text' value={userID} placeholder="User ID" onChange={onChangeUserID}/>
+            <input type='text' value={userPW} placeholder="User Password" onChange={onChangePasswd}/>
             <button onClick={onClickLogin}>Login</button>
         </form>
     </>
