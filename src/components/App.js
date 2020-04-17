a35746d9d3a0020901a000349099591f3bdc8152
jinwoo1225 <jinwoo17962@gmail.com>
2020-04-17 13:26:31 +0900

    App.js got redux dispatch to addclass

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom'
+import { connect } from 'react-redux';
 import Navigation from '../components/Navigation'
 import Home from '../routes/Home'
 import About from '../routes/About';
@@ -7,21 +8,21 @@ import Class from '../routes/Class';
 import Login from '../routes/Login';
 import Register from '../routes/Register';
 import MakeClass from '../routes/MakeClass';
+import { addClass } from '../store';
 import Axios from 'axios';
 
 
-function App() {
+function App({dispatchClass}) {
   Axios.get("http://tutor2tutee.ddns.net:3000/class/all")
-        .then(response=>{
-            console.log(response)
-            console.log(response.data)
-            console.log(response.data)
+        .then( async response=>{
+            console.log(response.data)  
+            await dispatchClass(response.data)        
         })
         .catch(error=>{
             console.log(error)
         })
         .then(()=>{
-            console.log("hello");
+            // console.log("hello");
         })
   return (
     <Router>
@@ -36,4 +37,10 @@ function App() {
   );
 }
 
-export default App;
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatchClass: classes => dispatch(addClass(classes))
+  }
+}
+
+export default connect(null, mapDispatchToProps) (App);
