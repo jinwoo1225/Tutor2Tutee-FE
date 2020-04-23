d176572ba4d737e844170defa4a6f655cfa17c3f
jinwoo1225 <jinwoo17962@gmail.com>
2020-04-24 01:20:04 +0900

    reformatted and simplified code

--- src/routes/Class.js
+++ src/routes/Class.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import { connect } from 'react-redux'
-import {Container} from 'react-bootstrap'
-import ShowClass from '../components/ShowClass.js'
+import {Container, Tab, Tabs, ProgressBar} from 'react-bootstrap'
 
 function Class(props){
     const {match:{params:{id}}, classState, history} = props 
     const _class = classState.filter(_class => id === _class._id)[0]
     console.log(history)
+
+    const [key, setKey] = useState("overview");
+    console.log(props)
+    if(_class === undefined){
+        // prop을 받지 못한다면 home으로 리턴
+        alert("잘못된 접근입니다.");
+        history.push('/');
+        return null;
+    }
     return(
     <Container>
-        <ShowClass _class={_class} history={history}/>
+        <div>
+            <h1>{_class.className}</h1>
+            <p>ClassType : {_class.classType} Category : {_class.category}<br/>Tutor : {_class.tutor}</p>
+            <Tabs
+                id="controlled-tab"
+                activeKey={key}
+                onSelect={e => setKey(e)}
+            >
+                <Tab eventKey="overview" title="개요">
+                    <h2>개요</h2>
+                    <p>{_class.studyAbout}</p>
+                </Tab>
+                <Tab eventKey="attendance" title="출결">
+                    <h2>현재 진행 상황(60%)</h2>
+                    <ProgressBar now={60} />
+                </Tab>
+                <Tab eventKey="note" title="강의노트">
+                    <h2>강의노트</h2>
+                    <p>{_class.lectureNote}</p>
+                </Tab>
+
+                <Tab eventKey="question" title="질의응답">
+                    <h2>질의응답</h2>
+                    <p>{_class.qna}</p>
+                </Tab>
+                
+
+            </Tabs>
+        </div>
     </Container>
     )
 }
--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,18 +1,61 @@
-import React from 'react';
-import { Container } from 'react-bootstrap'
-import LoginForm from '../components/LoginForm'
+import React, {useState} from 'react';
+import jQuery from 'jquery'
+import { Form, Button, Container } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { login, updateUser } from "../store"
+import { URL, checkAuth } from '../components/App';
 
-function Login(props) {
-    const {history} = props;
+function Login({dispatchUser, history}) {
+    const [userID, setUserID] = useState("");
+    const [userPW, setPassword] = useState("");
+
+    const onClickLogin = () => {
+        //ajax로 날릴 데이터
+        const data = 'username=' + userID + '&password=' + userPW;
+
+        jQuery.ajax({
+            type: "POST",
+            url: URL + "auth/login",
+            data : data,
+            dataType: "text",
+            success: (res)=>{
+                if(res === 'success'){
+                    console.log('로그인 성공')
+                    checkAuth({dispatchUser});
+                    history.push("/")
+                }else{alert("로그인 실패")}
+            },
+            error: ( xhr )=>{ console.log(xhr); }
+        })
+    };
+
     return(
-    <Container>
-        <LoginForm history={history}>
-            
-        </LoginForm>
-    </Container>
+        <Container>
+        <Form className="mt-md-3">
+            <Form.Group>
+                <Form.Label>아이디</Form.Label>
+                <Form.Control placeholder="아이디" onChange={e =>{setUserID(e.target.value)}}/>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>비밀번호</Form.Label>
+                <Form.Control type="password" placeholder="비밀번호" onChange={e =>{setPassword(e.target.value)}}/>
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="비밀번호 저장" />
+            </Form.Group>
+            <Button className="my-md-3" onClick={onClickLogin}>로그인</Button>
+            <p> 아이디가 없으신가요?<span role="img" aria-label="sweat">😅</span> : <Link to='/user/register'><Button className="btn-light">회원가입</Button></Link></p>
+            </Form>
+        </Container>
     )
 }
 
+function mapDispatchToProps(dispatch){
+    return {
+        dlogin: data=>dispatch(login(data)),
+        dispatchUser : data=>dispatch(updateUser(data))
+    }
+}
 
-
-export default (Login);
+export default connect(null, mapDispatchToProps) (Login);
