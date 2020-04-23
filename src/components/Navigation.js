eda86d0de590db17022564886012029eb191119b
jinwoo1225 <jinwoo17962@gmail.com>
2020-04-24 00:21:54 +0900

    fixed

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,7 +7,6 @@ import { connect } from 'react-redux';
 
 
 function Navigation({loginState, dlogout}){
-    console.log(loginState.user.nickname)
     return(
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
     <Link to='/'><Navbar.Brand >Tutor 2 Tutee</Navbar.Brand></Link>
@@ -47,32 +46,3 @@ function mapDispatchToProps(dispatch){
 }
 
 export default connect(mapStateToProps, mapDispatchToProps) (Navigation);
-// export default Navigation;
-
-
-
-
-// const [classID, setClassID] = useState("");
-    
-
-// const onChangeClassID = e => {setClassID(e.target.value);}
-
-// const onClickLogout = () => {dlogout()};
-
-// {/* <h3>This is Navigation Bar </h3>
-//             <p>Hello {loginState.id}</p>
-//             <form>
-//                 {/* 클래스로 이동 */}
-//                 {/* <input type='text' value={classID} placeholder="Class ID" onChange={onChangeClassID}/>
-//                 <Link to={`/class/${classID}`}><button>Class</button></Link>
-//             </form>
-            
-//             <button onClick={onClickLogout}>Logout</button>
-
-//             <Link to={'/'}><button>Home</button></Link>
-//             <Link to={'/about'}><button>About</button></Link>
-//             <Link to={'/login'}><button>loginPage</button></Link>
-//             <Link to={'/register'}><button>registerPage</button></Link>
-//             <Link to={'/makeclass'}><button>MakeClass</button></Link>
-
-//              */} */}
