b523ff159bf43480ee1f2b1804b9ed23e8034284
jinwoo1225 <jinwoo17962@gmail.com>
2020-05-25 17:59:35 +0900

    added start attendance

--- src/routes/Class.js
+++ src/routes/Class.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Axios from "axios";
-import { Container, Button, Card } from "react-bootstrap";
+import { Container, Button, Card, Form, InputGroup } from "react-bootstrap";
 import ClassTab from "../components/ClassTab";
 
 import {
@@ -12,17 +12,6 @@ import {
 } from "../components/App";
 import { Link } from "react-router-dom";
 
-const joinClass = (id, setClass) => {
-  Axios.get(URL + "class/" + id + "/join").then((response) => {
-    if (response.data === "fail") {
-      alert("문제가 있네요!");
-    } else {
-      alert("정상적으로 수강 신청했어요!");
-      setClass({ classLoaded: false });
-    }
-  });
-};
-
 function Class({
   match: {
     params: { id },
@@ -30,7 +19,6 @@ function Class({
   history,
 }) {
   const [_class, setClass] = useState({ classLoaded: false });
-  const [tutorName, setTutorName] = useState("");
   const [classState, setClassState] = useState();
   const [classTypeNum, setClassType] = useState();
   const [user, setUser] = useState({ _id: undefined });
@@ -38,22 +26,23 @@ function Class({
 
   if (_class.classLoaded === false) {
     Axios.get(URL + "class/" + id).then((response) => {
-      response.data === "fail" ? history.push("/") : setClass(response.data);
+      if (response.data === "fail") {
+        history.push("/");
+      } else {
+        setClass(response.data);
+        setClassState(statesRaw.indexOf(response.data.state));
+        console.log(response.data);
+        setClassType(classTypesRaw.indexOf(response.data.classType));
+      }
     });
     setClass({ classLoaded: true });
-    setTutorName("");
-  }
-
-  if (_class._id === id && tutorName === "") {
-    Axios.get(URL + "user/" + _class.tutor).then((response) => {
-      setTutorName(response.data.nickname);
-    });
-    setTutorName("Loading");
-    setClassState(statesRaw.indexOf(_class.state));
-    setClassType(classTypesRaw.indexOf(_class.classType));
   }
 
   if (user._id === undefined) {
+    checkAuth();
+  }
+
+  function checkAuth() {
     Axios.get(URL + "auth/isAuthenticated").then((response) => {
       response.data !== "fail" ? setUser(response.data) : setUser({ _id: "" });
     });
@@ -64,6 +53,32 @@ function Class({
     Axios.get(URL + "class/" + id + "/attendance").then((response) => {
       setAttenCode(response.data);
       alert("출석이 시작되었습니다!" + response.data);
+    });
+  }
+
+  function getAttendance() {
+    Axios.post(URL + "class/" + id + "/attendance", { auth: attenCode }).then(
+      (response) => {
+        alert(response.data);
+      }
+    );
+  }
+
+  function startClass() {
+    Axios.get(URL + "class/" + id + "/start").then((response) => {
+      alert(response.data);
+    });
+  }
+
+  function joinClass() {
+    Axios.get(URL + "class/" + id + "/join").then((response) => {
+      if (response.data === "fail") {
+        alert("문제가 있네요!");
+      } else {
+        alert("정상적으로 수강 신청했어요!");
+        setClass({ classLoaded: false });
+        checkAuth();
+      }
     });
   }
 
@@ -78,7 +93,7 @@ function Class({
                 ClassType : {classTypes[classTypeNum]} Category :{" "}
                 {_class.category}
                 <br />
-                Tutor : {tutorName}
+                Tutor : {_class.tutorNickName}
                 <br />
                 State : {states[classState]}
                 <br />
@@ -94,19 +109,39 @@ function Class({
                     <Button>설정하기</Button>
                   </Link>
                   <Button onClick={startAttendance}>출석하기</Button>
+                  {[1].includes(classState) ? (
+                    <Button onClick={startClass}>강의 마감하기</Button>
+                  ) : null}
                 </>
-              ) : classState === 1 ? (
+              ) : user._id === "" ? null : [1].includes(classState) ? ( //게스트인 경우
                 // 내가 튜티이면
                 //강의를 개설할 준비가 되면
                 <>
                   <p>내가 튜티네요!</p>
-                  <Button
-                    onClick={() => {
-                      joinClass(_class._id, setClass);
-                    }}
-                  >
-                    참가하기
-                  </Button>
+                  {user.classesAsTutee.includes(_class._id) ? (
+                    <>
+                      {classTypeNum !== undefined &&
+                      [0].includes(classTypeNum) ? (
+                        //온라인 실시간 수업일 경우의 인증번호
+                        <InputGroup>
+                          <InputGroup.Prepend>
+                            <InputGroup.Text>인증번호</InputGroup.Text>
+                          </InputGroup.Prepend>
+                          <Form.Control
+                            placeholder="인증번호는 튜터에게 물어보세요!!"
+                            onChange={(e) => setAttenCode(e.target.value)}
+                          ></Form.Control>
+                          <InputGroup.Append>
+                            <Button onClick={getAttendance}>출석하기</Button>
+                          </InputGroup.Append>
+                        </InputGroup>
+                      ) : (
+                        <Button onClick={getAttendance}>출석하기</Button>
+                      )}
+                    </>
+                  ) : (
+                    <Button onClick={joinClass}>참가하기</Button>
+                  )}
                 </>
               ) : (
                 <Button disabled>완료되었습니다.</Button>
