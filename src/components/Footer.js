219d8cf6c0cb012dac31d277981621941254bc6a
jinwoo1225 <jinwoo17962@gmail.com>
2020-06-04 02:53:19 +0900

    제작자와 형식 추가

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Footer() {
-  return <h1>This is Footer</h1>;
+  return (
+    <h5 className="text-center">
+      한경대학교 2020 문제해결프로젝트3 <br />
+      제작자 : 홍진우, 변우섭, 강명조
+    </h5>
+  );
 }
 
 export default Footer;
