64a16ca7a9af11a86871d500f14c43c19122da3f
jinwoo1225 <jinwoo17962@gmail.com>
2020-06-04 02:53:50 +0900

    카드 보여주는 영역중 강의 형태 추가

--- src/components/CardShow.js
+++ src/components/CardShow.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card, Button, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { classTypesRaw, classTypes } from "./App";
 
 function CardShow({ _class, col }) {
   //보여줄 카드의 정보 컴포넌트
@@ -8,7 +9,10 @@ function CardShow({ _class, col }) {
     <Col md={col} className="my-3" key={_class._id}>
       <Card body>
         <Card.Title>{_class.className}</Card.Title>
-        <Card.Text>{_class.tutorNickName}</Card.Text>
+        <Card.Subtitle>
+          강의유형 : {classTypes[classTypesRaw.indexOf(_class.classType)]}
+        </Card.Subtitle>
+        <Card.Text>튜터 : {_class.tutorNickName}</Card.Text>
         <Link to={`class/id/${_class._id}`}>
           <Button block>자세히</Button>
         </Link>
