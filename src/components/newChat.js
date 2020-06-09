9f59fc3fc3e66a5954243bf2d8237ca9e9cf5884
jinwoo1225 <jinwoo17962@gmail.com>
2020-06-09 20:26:06 +0900

    새로운 UI 적용중

--- src/components/newChat.js
+++ src/components/newChat.js
@@ -58,17 +58,64 @@ function NewChat({ classInfo, userInfo }) {
         >
           {chat.map(({ username, time, message, system }, index) => {
             return (
-              <Card
-                body
+              <div
                 key={index}
                 style={{
-                  float: username === userInfo.nickname ? "right" : "left",
+                  height: "auto",
+                  width: "100%",
                 }}
               >
-                {message}
-              </Card>
+                <div style={{ float: "right" }}>
+                  <div>{username}</div>
+                  <div>
+                    <div style={{ display: "inline" }}>{time}</div>
+                    <div
+                      style={{
+                        display: "inline",
+                        borderRadius: "10px",
+                        padding: "5px",
+                        border: "1px solid Red",
+                      }}
+                    >
+                      {message}
+                    </div>
+                  </div>
+                </div>
+              </div>
             );
           })}
+
+          <div
+            style={{
+              height: "auto",
+              width: "100%",
+            }}
+          >
+            <div style={{ float: "right" }}>
+              <div>user.name</div>
+              <div>
+                <div style={{ display: "inline" }}>time</div>
+                <div
+                  style={{
+                    display: "inline",
+                    borderRadius: "10px",
+                    padding: "5px",
+                    border: "1px solid Red",
+                  }}
+                >
+                  잘 있슈
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              float: "left",
+              width: "auto",
+            }}
+          >
+            안녕하슈
+          </div>
           <InputGroup>
             <Form.Control
               className="MessageSendForm"
